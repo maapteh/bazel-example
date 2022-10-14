8abd246d7d3431b94e31279b4b656e68d774e5ec
Maapteh <maarten@oudenniel.nl>
2022-10-14 13:24:13 +0200

    fix: remove logging

--- packages/apps/example/src/index.tsx
+++ packages/apps/example/src/index.tsx
@@ -1,8 +1,6 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-
-console.log(9);
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
